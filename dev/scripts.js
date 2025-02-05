b6ddd9a03c6c75f251495f394230215ff65d585b
Rafael <campossepulvedar@gmail.com>
2025-02-04 23:17:28 -0300

    feat: resizing images

--- dev/scripts.js
+++ dev/scripts.js
@@ -8,3 +8,31 @@ menuIcon.addEventListener("click", () => {
   content.classList.toggle("active");
   header.classList.toggle("active");  // Añadimos la clase active al header
 });
+
+/* ANIMACIONES */
+window.addEventListener('DOMContentLoaded', () => {
+  const contents = document.querySelectorAll('.initial-fade-in');
+  
+  // Activar el efecto en los elementos que ya están en la pantalla de inicio
+  contents.forEach(content => {
+    const contentPosition = content.getBoundingClientRect().top;
+    const windowHeight = window.innerHeight;
+
+    if (contentPosition < windowHeight * 0.8) {
+      content.classList.add('visible');
+    }
+  });
+});
+window.addEventListener('scroll', () => {
+  const contents = document.querySelectorAll('.fade-in');
+  
+  contents.forEach(content => {
+    const contentPosition = content.getBoundingClientRect().top;
+    const windowHeight = window.innerHeight;
+
+    // Si el contenido entra en la vista mientras se hace scroll, agregar la clase 'visible'
+    if (contentPosition < windowHeight * 0.8) {
+      content.classList.add('visible');
+    }
+  });
+});
